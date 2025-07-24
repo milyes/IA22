3c97cb9051c14565bbad4ed22f3ea519d4cc1818
zoubirou ilyes <119434103+milyes@users.noreply.github.com>
2025-07-24 12:53:09 -0400

    Create menu.js
    Signed-off-by: zoubirou ilyes <119434103+milyes@users.noreply.github.com>

---
+++ menu.js
@@ -0,0 +1,10 @@
+<nav>
+  <a href="index.html">Accueil</a>
+  <a href="ia22_logic.html">Logic</a>
+  <a href="ia22_sante.html">Santé</a>
+  <a href="ia22_transport.html">Transport</a>
+  <a href="ia22_environnement.html">Environnement</a>
+  <a href="ia22_energie.html">Énergie</a>
+  <a href="ia22_securite.html">Sécurité</a>
+  <a href="ia22_art.html">Art</a>
+</nav>
